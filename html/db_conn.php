52cfac162eafd82a5ea382342150d509cc3c3ee3
TheCountt <isaacadedeji0@yahoo.com>
2021-09-25 00:44:29 +0100

    commit changes

---
+++ html/db_conn.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "mysqlserverhost";
+$username = "mysql_user";
+$password = "1234ABC";
+$dbname = "toolingdb";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+?>
